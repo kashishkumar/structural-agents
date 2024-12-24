45605ac81ca8716367077ce2fb3ee42d4ffd31ee
Kashish Kumar <34455806+kashishkumar@users.noreply.github.com>
2024-12-24 20:54:34 +0530

    Initialised project and add basic beam design agent

---
+++ structural-agents.py
@@ -0,0 +1,35 @@
+from phi.agent import Agent
+from phi.model.openai import OpenAIChat
+from markdown import Markdown
+from mdx_math import MathExtension
+from yaml import safe_load
+
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+# Create custom markdown renderer with LaTeX support
+md = Markdown(extensions=["mdx_math"])
+
+
+def load_config(path):
+    with open(path) as f:
+        return safe_load(f)
+
+
+config = load_config("config.yaml")
+
+beam_design_agent = Agent(
+    name=config["beam_design_agent"]["name"],
+    model=OpenAIChat(id=config["beam_design_agent"]["model"]),
+    tools=[],
+    instructions=config["beam_design_agent"]["instructions"],
+    show_tool_calls=True,
+    markdown=True,
+    markdown_renderer=md,
+)
+
+beam_design_agent.print_response(
+    config["beam_design_agent"]["query"],
+    stream=False,
+)
