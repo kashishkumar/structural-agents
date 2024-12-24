0fc61e17504dd3fa651dd831ec5e908224b00ef7
Kashish Kumar <34455806+kashishkumar@users.noreply.github.com>
2024-12-24 22:15:34 +0530

    Added response model and modified pydantic models

--- models.py
+++ models.py
@@ -4,10 +4,10 @@ from pydantic import BaseModel
 class beam_design_input(BaseModel):
     span: float
     load: float
+    load_type: str
     material: str
     initial_section: str
 
 class beam_design_output(BaseModel):
-    material: str
     section: str
 
--- structural_agents.py
+++ structural_agents.py
@@ -1,6 +1,10 @@
 from phi.agent import Agent
 from phi.model.openai import OpenAIChat
 from yaml import safe_load
+# Import calculation tools
+from phi.tools.calculator import Calculator
+from phi.tools.python import PythonTools
+from models import beam_design_input, beam_design_output
 
 from dotenv import load_dotenv
 
@@ -16,10 +20,11 @@ config = load_config("config.yaml")
 beam_design_agent = Agent(
     name=config["beam_design_agent"]["name"],
     model=OpenAIChat(id=config["beam_design_agent"]["model"]),
-    tools=[],
+    tools=[Calculator(), PythonTools()],
     instructions=config["beam_design_agent"]["instructions"],
     show_tool_calls=True,
     markdown=True,
+    response_model=beam_design_output,
 )
 
 
